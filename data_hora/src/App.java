243fbcd9c3f1e18d9f8e12ea730728ccb4296944
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-03-06 12:03:27 -0300

    Resolvendo conflito git

--- data_hora/src/App.java
+++ data_hora/src/App.java
@@ -5,7 +5,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         /*
          * Conceitos importantes
          * 
@@ -166,5 +166,30 @@ public class App {
         System.out.println("d06 = " + fmt5.format(d06));
         // imprime no formato iso
         System.out.println("d06 = " + d06.toString());
+        System.out.println();
+
+        // Converter data hora Global para local
+        // data-hora global, timezone(sistema local)
+        ZoneId.getAvailableZoneIds(); // lista todos os zoneids disponíveis;
+
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+
+        System.out.println();
+
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 mês = " + d04.getMonthValue());
+        System.out.println("d04 ano = " + d04.getYear());
+
+        System.out.println("d05 hora = " + d05.getHour());
+        System.out.println("d05 minuto(s) = " + d05.getMinute());
+        System.out.println("d05 segundo(s) = " + d05.getSecond());
+
     }
 }
